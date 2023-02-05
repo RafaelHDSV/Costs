5cf9bd687c5ee03f7ca26bbf39b64682c8def8ef
RafaelHDSV <rafaebolt@gmail.com>
2023-02-05 12:35:39 -0300

    Adding link for routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
@@ -8,10 +8,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <ul>
-          <li>Home</li>
-          <li>Contato</li>
-        </ul>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/company'>Company</Link>
+          <Link to='/contact'>Contact</Link>
+          <Link to='/newproject'>NewProject</Link>
+        </div>
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/company' element={<Company />} />
