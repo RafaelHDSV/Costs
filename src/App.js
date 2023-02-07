a3eae3ea58731e8d681474408222cbfc5b0ebe13
RafaelHDSV <rafaebolt@gmail.com>
2023-02-07 17:26:44 -0300

    Adding components

--- src/App.js
+++ src/App.js
@@ -6,18 +6,13 @@ import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Link to='/'>Home</Link>
-          <Link to='/company'>Company</Link>
-          <Link to='/contact'>Contact</Link>
-          <Link to='/newproject'>NewProject</Link>
-        </div>
-
         <Container customClass='min-height'>
           <Routes>
             <Route exact path='/' element={<Home />} />
