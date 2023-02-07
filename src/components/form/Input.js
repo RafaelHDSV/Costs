66bfa0e9c798f9a17f4a17909e0ea6eb658d4550
RafaelHDSV <rafaebolt@gmail.com>
2023-02-07 18:44:39 -0300

    Adding input styles

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -4,8 +4,8 @@ import styles from './Input.module.css'
 const Input = ({ type, text, name, placeholder, handleOnChange, value }) => {
     return (
         <div className={styles.form_control}>
-            <label htmlFor=""></label>
-            <input type="text" />
+            <label htmlFor={name}>{text}:</label>
+            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnChange} value={value} />
         </div>
     )
 }
