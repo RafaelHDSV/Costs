f0a1f38b0c63c7c0a0c8ce5fc8b6d1fc10598b5b
RafaelHDSV <rafaebolt@gmail.com>
2023-02-07 17:56:25 -0300

    Updating footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,12 +5,14 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <footer>
-            <ul>
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
                 <li><FaFacebook></FaFacebook></li>
                 <li><FaInstagram></FaInstagram></li>
                 <li><FaLinkedin></FaLinkedin></li>
             </ul>
+
+            <p className={styles.copy_right}><span>Costs</span> &copy; 2023</p>
         </footer>
     )
 }
