653267a728da5f5d9cc44a97d96c789e2af8652b
RafaelHDSV <rafaebolt@gmail.com>
2023-02-07 18:00:30 -0300

    Updating home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import savings from '../../images/savings.svg'
+
+import styles from './Home.module.css'
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <section>
+            <h1>Bem-Vindo ao <span>Costs</span></h1>
+            <p>Começe a gerenciar os seus projetos agora mesmo!</p>
+            <a href="">Criar Projeto</a>
+            <img src={savings} alt="savings.svg'" />
+        </section>
     )
 }
 
