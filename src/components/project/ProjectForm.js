2129b8b3707cb364d12349d5d44b621a9b25abaf
RafaelHDSV <rafaebolt@gmail.com>
2023-02-07 18:53:02 -0300

    Adding submit button

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Input from '../form/Input'
+import Select from '../form/Select'
 
 import styles from './ProjectForm.module.css'
 
@@ -9,9 +10,7 @@ const ProjectForm = () => {
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do Projeto'></Input>
             <Input type='number' text='Orçamento do projeto' name='budget' placeholder='Insira o orçamento total'></Input>
 
-            <select name="category_id">
-                <option disabled selected>Selecione a categoria</option>
-            </select>
+            <Select name='category_id' text='Selecione a categoria'></Select>
 
             <input type="submit" value="Criar Projeto" />
         </form>
