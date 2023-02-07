314af00c6e22acc60ab8f51c19bb60429061da71
RafaelHDSV <rafaebolt@gmail.com>
2023-02-07 18:19:25 -0300

    Updating home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <section className={styles.home_container}>
             <h1>Bem-Vindo ao <span>Costs</span></h1>
             <p>Começe a gerenciar os seus projetos agora mesmo!</p>
-            <LinkButton></LinkButton>
+            <LinkButton to='/newproject' text='Criar Projeto'></LinkButton >
             <img src={savings} alt="savings.svg'" />
         </section>
     )
