2ad5d05bd5ae895036c4211c342eabb53d003227
RafaelHDSV <rafaebolt@gmail.com>
2023-02-07 18:39:00 -0300

    Adding project form

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './ProjectForm.module.css'
 
 const ProjectForm = () => {
     return (
-        <form>
+        <form className={styles.form}>
             <input type="text" placeholder='Insira o nome do projeto' />
             <input type="number" placeholder='Insira o orçamento total' />
 
             <select name="category_id">
-                <option disabled>Selecione a categoria</option>
+                <option disabled selected>Selecione a categoria</option>
             </select>
 
             <input type="submit" value="Criar Projeto" />
