28d8c60bc7049e6f07b9ba2b72e17d31132f2678
RafaelHDSV <rafaebolt@gmail.com>
2023-02-09 18:59:13 -0300

    Updating form

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
