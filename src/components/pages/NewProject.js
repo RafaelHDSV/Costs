8bb73c07158ab4147dd126f065affdffdd36a235
RafaelHDSV <rafaebolt@gmail.com>
2023-02-09 19:05:02 -0300

    Adding backend

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import ProjectForm from '../project/ProjectForm'
 
 import styles from './NewProject.module.css'
 
 const NewProject = () => {
+
+    const history = useHistory()
+
+    function createPost(project) {
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+    }
+
     return (
         <div className={styles.new_project_container}>
             <h1>Criar Projeto</h1>
