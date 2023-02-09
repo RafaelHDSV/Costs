32213a5172cb6caa7b6b5ceb9fc46603e0896bab
RafaelHDSV <rafaebolt@gmail.com>
2023-02-09 19:08:08 -0300

    Adding backend

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -12,6 +12,18 @@ const NewProject = () => {
         // initialize cost and services
         project.cost = 0
         project.services = []
+
+        fetch('http:/localhost:5000/projects', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        }).then((resp) => resp.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => console.log(err))
     }
 
     return (
