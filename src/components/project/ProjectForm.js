ead21697136723d79fbaf2f16ed360cc89a91f91
RafaelHDSV <rafaebolt@gmail.com>
2023-02-09 18:53:00 -0300

    Updating project form

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
@@ -6,6 +7,21 @@ import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 const ProjectForm = ({ btnText }) => {
+
+    const [categories, setCategories] = useState([])
+
+    fetch('http://localhost:5000/categories', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+
     return (
         <form className={styles.form}>
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do Projeto'></Input>
