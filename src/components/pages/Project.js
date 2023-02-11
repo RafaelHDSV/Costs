388002fb5956d1cf7730bdfd23e9d25e5acd74ea
RafaelHDSV <rafaebolt@gmail.com>
2023-02-11 17:24:07 -0300

    Adding edit button

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 import styles from './Project.module.css'
 
 const Project = () => {
+
+    const { id } = useParams()
+    const [project, setProject] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json'
+            },
+        }).then((resp) => resp.json())
+            .then((data) => {
+                setProject(data)
+            })
+            .catch((err) => console.log(err))
+    }, [id])
+
     return (
-        <div>Project</div>
+        <div>{project.name}</div>
     )
 }
 
